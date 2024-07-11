4e20b748b266040dcf6eb3d209d5e0bb6fa03518
Chris <173185832+odinca@users.noreply.github.com>
2024-07-11 16:16:56 +0100

    Write initial algorithm as pseudocode

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Function to randomly return a choice from computer
+
+// Request choice from player as input
+
+// Store player's score
+
+// Function to play a round, comparing players choice with the computer's
+
+// Logic to play a best-of five rounds
