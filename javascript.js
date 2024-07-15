a4780d148653cbe44f9c64c5fb22b675e46332bc
Chris <173185832+odinca@users.noreply.github.com>
2024-07-15 10:54:53 +0100

    First iteration of function to receive users choice

--- javascript.js
+++ javascript.js
@@ -1,22 +1,41 @@
 // Function to randomly return a choice from computer
 function getComputerChoice(){
-    let choice
+    let computerChoice
     const randInt = Math.floor(Math.random() * 3) + 1;
     if (randInt === 1) {
-        choice = "Rock";
+        computerChoice = "Rock";
     }
     else if (randInt === 2) {
-        choice = "Paper";
+        computerChoice = "Paper";
     }
     else {
-        choice = "Scissors";
+        computerChoice = "Scissors";
     }
-    return choice
+    return computerChoice
 }
 
-console.log(getComputerChoice())
-
 // Request choice from player as input
+
+function getHumanChoice(){
+    let humanChoice
+    let n = 1;
+    while (n === 1) {
+        humanChoice = Number(prompt(`Choose your option:
+            1 = Rock
+            2 = Paper
+            3 = Scissors`));
+        if (isNaN(humanChoice) || humanChoice < 0 || humanChoice > 3) {
+            n = 1;
+            console.log("Invalid choice. Please try again")
+        }
+        else {
+            n = 2;
+        }
+    }
+    return humanChoice
+}
+
+console.log(getHumanChoice())
 
 // Store player's score
 
