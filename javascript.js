e6de95b364cad15ff1fdad9ae35a94db33741b87
Chris <173185832+odinca@users.noreply.github.com>
2024-07-15 09:11:25 +0100

    Write function for computer to randomly return a choice

--- javascript.js
+++ javascript.js
@@ -1,4 +1,20 @@
 // Function to randomly return a choice from computer
+function getComputerChoice(){
+    let choice
+    const randInt = Math.floor(Math.random() * 3) + 1;
+    if (randInt === 1) {
+        choice = "Rock";
+    }
+    else if (randInt === 2) {
+        choice = "Paper";
+    }
+    else {
+        choice = "Scissors";
+    }
+    return choice
+}
+
+console.log(getComputerChoice())
 
 // Request choice from player as input
 
